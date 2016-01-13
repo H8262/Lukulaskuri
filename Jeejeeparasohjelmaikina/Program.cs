735d5d631c41100e1a78869ef228c0bab019f5e7
H8262 <h8262@student.jamk.fi>
2016-01-13 15:25:43 +0200

    Jeejee

---
+++ Jeejeeparasohjelmaikina/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeejeeparasohjelmaikina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int muuttuja = 0;
+            int luku = 0;
+
+            do
+            {
+                Console.Write("Anna luku: ");
+                luku = int.Parse(Console.ReadLine());
+
+                muuttuja = muuttuja + luku;
+
+            } while (luku != 0);
+
+            Console.WriteLine("Lukujen summa on: " + muuttuja);
+        }
+    }
+}
